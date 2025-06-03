0c03642c777268dc9ce0ccaa54543ad6768e138c
AnastasiyaShagrai <shagrai_anastasiya13@mail.ru>
2025-06-03 23:16:26 +0300

    Структура проекта + класс Toy

---
+++ src/Toy.java
@@ -0,0 +1,29 @@
+public class Toy {
+     private int id;
+     private String name;
+     private int weight;
+
+     public Toy(int id, String name, int weight) {
+          this.id = id;
+          this.name = name;
+          this.weight = weight;
+     }
+
+     public int getId() {
+          return id;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public int getWeight() {
+          return weight;
+     }
+
+     @Override
+     public String toString() {
+          return name + " (id=" + id + ", weight=" + weight + ")";
+     }
+}
+ 
